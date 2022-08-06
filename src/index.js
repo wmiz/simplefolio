a5a3f90f4c8782b417f912b9bbfe60b9d405541e
wmiz <will.misback@gmail.com>
2022-08-06 16:12:00 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import simpleParallax from "simple-parallax-js";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
